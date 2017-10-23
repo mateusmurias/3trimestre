8a380fbdc1bbad6dd8b612b8a06d78b3f1d2740d
mateusmurias <32718229+mateusmurias@users.noreply.github.com>
2017-10-23 16:01:20 -0200

    3 dia
    botando o html junto com o php

--- portuga 1info1/cabecalho.php
+++ portuga 1info1/cabecalho.php
@@ -1,8 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/completo.css">
+	<link rel="stylesheet" type="text/css" href="css/geral.css">
+	<link rel="stylesheet" type="text/css" href="css/profs.css">
+	<title>Todos professores</title>
+	<meta charset="utf-8">
 </head>
 <body>
-	<h1> Tarefas - 1info</h1>
+	<!-- cabeçalho -->
+	<header>
+		<img src="imagens/logo.png" id="logo"/>
+		<h1 id="nome_site">Agenda de tarefas</h1>
+	</header>
+	<div class='divider'></div>
+	<!-- menu -->
+	
+	<nav class="menu">
+		<a href=""><section class="item menu">Próximas tarefas</section></a>
+		<a href=""><section class="item menu">Todas Tarefas</section></a>
+		<a href=""><section class="item menu">Tarefas por disciplina</section></a>
+		<a href=""><section class="item menu">Tarefas por data</section></a>
+		<a href=""><section class="item menu ativo">Professores</section></a>
+		<a href=""><section class="item menu">Colegas</section></a>
+	</nav>
+	<div class='divider'></div>
 
+	<!-- colunas para centralizar -->
+	<div class="coluna10">.</div>
--- portuga 1info1/listaprofessores.php
+++ portuga 1info1/listaprofessores.php
@@ -1,3 +1,32 @@
 <?php
 
-include('cabecalho.php');
+include("cabecalho.php");
+include("professores.php");
+?>
+	<!-- conteudo principal -->
+	<article class="coluna80">
+			<section class="lista">
+		<ul>
+<?php
+
+$lista = listaProfessores();
+
+foreach ($lista as $professor) {
+
+?>
+<li><a href="detalhaProfessor.php?cod=<?=$professor['siape'] ?>"><?=$professor['nome'] ?></a></li>
+
+<?php
+ }
+
+?>
+
+		</ul>
+	</section>
+</article>
+
+<?php
+
+include("rodape.php")
+
+?>
